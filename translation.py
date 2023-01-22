1a4cbb383936cf5f45e2d1b67d9e9c6801010980
ShareMe07 <123290520+ShareMe07@users.noreply.github.com>
2023-01-22 13:30:03 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 START_MESSAGE = '''**Hello, {}
-I Am ShortnerFly.com, Bulk Link Converter. I Can Convert Links Directly From Your ShortnerFly Account,
+I Am MdiskShortner's.in, Bulk Link Converter. I Can Convert Links Directly From Your ShortnerFly Account,
     
 1. Go To 👉 https://ShortnerFly.com/member/tools/api  
 2. Than Copy API Key
