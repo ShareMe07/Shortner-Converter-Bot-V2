07fe7b0d3bfc23892f20075aedcc71eb561812e5
ShareMe07 <123290520+ShareMe07@users.noreply.github.com>
2023-01-22 13:36:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Mandatory variables for the bot to start
 API_ID = int(os.getenv("API_ID", "Your Api Id"))
 API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5800276525:AAE5kJnEVgYxVh3qF62KHahVMXFEavflK2I")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
